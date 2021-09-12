6f2c53d794641691f35a63bdfb223395ee035cc7
tiagosatur <tiagosatur@gmail.com>
2021-09-12 20:30:54 -0300

    Add post categories route, model and repository

---
+++ src/repositories/categories.repositories.ts
@@ -0,0 +1,31 @@
+import { Category } from '../models/Category';
+
+// DTO = Data Transfer Object
+interface ICreateCategoryInput {
+  name: string;
+  description: string;
+}
+
+class CategoriesRepository {
+  private categories: Category[];
+
+  constructor() {
+    this.categories = [];
+  }
+
+  create({ name, description }: ICreateCategoryInput): Category {
+    const category = new Category();
+
+    Object.assign(category, {
+      name,
+      description,
+      created_at: new Date(),
+    });
+
+    this.categories.push(category);
+
+    return category;
+  }
+}
+
+export { CategoriesRepository };
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 
+import { categoriesRoutes } from './routes';
+
 const app = express();
 
-app.get('/', (req, res) => {
-  res.status(200).send('Root route exists');
-});
+app.use(express.json());
+
+app.use('/categories', categoriesRoutes);
 
 app.listen(3333);
