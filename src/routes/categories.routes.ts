1fea5b7f32a8ba997efd4ae867b2bc905011b49d
tiagosatur <tiagosatur@gmail.com>
2021-09-22 09:52:26 -0300

    Create cars module (folder)

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,10 +1,12 @@
 import { Request, Response, Router } from 'express';
 
-import { CategoriesRepository } from '../repositories/categories.repositories';
-import { CreateCategoryService } from '../services/create.category.service';
+import { CategoriesRepository } from '../modules/cars/repositories/categories.repository';
+import { PostgresCategoriesRepository } from '../modules/cars/repositories/postgres.categories.repository';
+import { CreateCategoryService } from '../modules/cars/services/create.category.service';
 
 const categoriesRoutes = Router();
 const categoriesRepository = new CategoriesRepository();
+const postgresCategoriesRepository = new PostgresCategoriesRepository();
 
 categoriesRoutes.post('/', (request: Request, response: Response) => {
   const { name, description } = request.body;
