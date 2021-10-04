ca730229582adeebbd184ea049363cb3d3f8d017
tiagosatur <tiagosatur@gmail.com>
2021-10-04 20:46:41 -0300

    Centralize routes in /routes/index.ts

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,2 +1,10 @@
-export * from './categories.routes';
-export * from './specifications.routes';
+import { Router } from 'express';
+
+import { categoriesRoutes } from './categories.routes';
+import { specificationRoutes } from './specifications.routes';
+
+const router = Router();
+router.use('/categories', categoriesRoutes);
+router.use('/specifications', specificationRoutes);
+
+export { router as routes };
--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,10 @@
 import express from 'express';
 
-import { categoriesRoutes, specificationRoutes } from './routes';
+import { routes } from './routes';
 
 const app = express();
 
 app.use(express.json());
-
-app.use('/categories', categoriesRoutes);
-app.use('/specifications', specificationRoutes);
+app.use(routes);
 
 app.listen(3333);
