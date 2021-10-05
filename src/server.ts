ac5580c4dcd162bcda5379c06aa1e0db164c1ca9
tiagosatur <tiagosatur@gmail.com>
2021-10-05 12:41:42 -0300

    Add swagger and create doc for create category route

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,13 @@
 import express from 'express';
+import swagger from 'swagger-ui-express';
 
 import { routes } from './routes';
+import swaggerConfig from './swagger.json';
 
 const app = express();
 
 app.use(express.json());
+app.use('/api/docs', swagger.serve, swagger.setup(swaggerConfig));
 app.use(routes);
 
 app.listen(3333);
